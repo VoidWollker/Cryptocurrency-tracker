d4085983cca34a298f954fcd1c8683256ebda373
Roman Nichi <Roman.Nichi.O@gmail.com>
2022-05-26 22:09:40 +0400

    work with Currency Api

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import axios from 'axios';
+import React, {useState, useEffect} from 'react';
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 
 import './App.css';
@@ -8,16 +9,35 @@ import CurrencyList from './components/ReviewPage/CurrencyList/CurrencyList';
 
 
 function App() {
+  const [coins, setCoins] = useState([]);
+
+  const getData = async () => {
+    try {
+      const btc = await axios.get('https://api.coingecko.com/api/v3/coins/bitcoin')
+      const eth = await axios.get('https://api.coingecko.com/api/v3/coins/ethereum')
+      const resCoins = [btc.data, eth.data]
+      console.log(resCoins);
+      setCoins(resCoins);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+
   return (
     <div className="App">
-      
       <BrowserRouter>
         <Header userName='Roman Nichipurenko' userPhoto=''/>
         <Routes>
           <Route path="/review"
             element={
               <CurrencyList 
-                currencyArrayProps={[{id: 1, name: 'BTC', price: 100}, {id: 2, name: 'ETH', price: 200}, {id: 3, name: 'USD', price: 150}]}
+                //currencyArrayProps={[{id: 1, name: 'BTC', price: 100}, {id: 2, name: 'ETH', price: 200}, {id: 3, name: 'USD', price: 150}]}
+                currencyArrayProps={coins}
               />
             }
           />
--- src/components/ReviewPage/CurrencyList/CurrencyList.jsx
+++ src/components/ReviewPage/CurrencyList/CurrencyList.jsx
@@ -4,16 +4,16 @@ import CurrencyItem from "../CurrencyItem/CurrencyItem";
 import'./CurrencyList.scss' 
 
 export default function CurrencyList({currencyArrayProps}) {
-    const [currencyArray, setCurrencyArray] = useState(currencyArrayProps)
+    const [currencyArray, setCurrencyArray] = useState([])
 
     return(
         <div className="currencyList">
         <h2>Current prices of coins</h2>    
-        {currencyArray.map((currency =>{
+        {currencyArrayProps.map((currency =>{
             return(
                 <CurrencyItem
                     labelText={currency.name}
-                    currencyPrice={currency.price}
+                    currencyPrice={currency.market_data.current_price.rub}
                     key={currency.id}
                 />
             )
