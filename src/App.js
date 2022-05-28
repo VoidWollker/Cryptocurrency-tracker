dec693209c14e85dd8542ad8aa6d23925cd54842
Roman Nichi <Roman.Nichi.O@gmail.com>
2022-05-28 10:56:16 +0400

    fix miport Routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, {useState, useEffect} from 'react';
-import {BrowserRouter, Route, Route} from 'react-router-dom'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 
 import './App.css';
 
